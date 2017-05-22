ab0a533944ba176e7f293e6447162d470656216e
Paul Jablonski <pauljablonski@me.com>
2017-05-22 21:37:08 +0100

    Add logic to Ranking class

--- app/src/main/java/com/codeclan/example/movierankings/Ranking.java
+++ app/src/main/java/com/codeclan/example/movierankings/Ranking.java
@@ -10,12 +10,38 @@ public class Ranking {
 
     private ArrayList<Movie> rankings;
 
-    public Ranking() {
-        this.rankings = new ArrayList<>();
+    public Ranking(){
+        this.rankings = new ArrayList<Movie>();
     }
 
 
-    public ArrayList<Movie> getRankings() {
-        return new ArrayList<>(this.rankings);
+    public void addMovie(Movie movie) {
+        this.rankings.add(movie);
     }
+
+    public int getNumberOfMovies() {
+        return this.rankings.size();
+    }
+
+    public Movie getMovieByRanking(int i) {
+        return rankings.get(i - 1);
+    }
+
+    public void replaceMovieAtEnd(Movie movie) {
+        int index = this.getNumberOfMovies() - 1;
+        this.rankings.set(index, movie);
+    }
+
+
+    public Movie findMovieByTitle(String title) {
+        Movie foundMovie = null;
+        for (Movie movie : this.rankings) {
+            if (movie.getTitle() == title) {
+                foundMovie = movie;
+            }
+        }
+        return foundMovie;
+    }
+
+
 }
--- app/src/test/java/com/codeclan/example/movierankings/RankingTest.java
+++ app/src/test/java/com/codeclan/example/movierankings/RankingTest.java
@@ -1,8 +1,11 @@
 package com.codeclan.example.movierankings;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+
 
 /**
  * Created by PaulJablonski on 22/05/2017.
@@ -10,12 +13,50 @@ import static org.junit.Assert.*;
 public class RankingTest {
 
     @Test
-    public void rankingsAreNotNull(){
+    public void canAddMovie(){
         Ranking rankingList = new Ranking();
-        assertNotNull(rankingList.getRankings());
+        Movie movie1 = new Movie("Requiem for a Dream", "Drama", 1);
+        rankingList.addMovie(movie1);
+        assertEquals(1, rankingList.getNumberOfMovies());
     }
 
     @Test
-    public void
+    public void canGetMovieByRanking(){
+        Ranking rankingList = new Ranking();
+        Movie movie1 = new Movie("Requiem for a Dream", "Drama", 1);
+        Movie movie2 = new Movie("American Beauty", "Drama", 2);
+        Movie movie3 = new Movie("Fight Club", "Action", 3);
+        rankingList.addMovie(movie1);
+        rankingList.addMovie(movie2);
+        rankingList.addMovie(movie3);
+        Movie returnedMovie = rankingList.getMovieByRanking(2);
+        assertEquals(3, rankingList.getNumberOfMovies());
+        assertEquals(movie2, returnedMovie);
+    }
+
+    @Test
+    public void canReplaceMovieAtEndOfList() {
+        Ranking rankingList = new Ranking();
+        Movie movie1 = new Movie("Requiem for a Dream", "Drama", 1);
+        Movie movie2 = new Movie("American Beauty", "Drama", 2);
+        Movie movie3 = new Movie("Fight Club", "Action", 3);
+        rankingList.addMovie(movie1);
+        rankingList.addMovie(movie2);
+        rankingList.addMovie(movie3);
+        rankingList.replaceMovieAtEnd(movie1);
+        assertEquals(movie1, rankingList.getMovieByRanking(3));
+    }
+
+    @Test
+    public void canFindMovieByTitle(){
+        Ranking rankingList = new Ranking();
+        Movie movie1 = new Movie("Requiem for a Dream", "Drama", 1);
+        Movie movie2 = new Movie("American Beauty", "Drama", 2);
+        Movie movie3 = new Movie("Fight Club", "Action", 3);
+        rankingList.addMovie(movie1);
+        rankingList.addMovie(movie2);
+        rankingList.addMovie(movie3);
+        assertEquals(movie2, rankingList.findMovieByTitle("American Beauty"));
+    }
 
 }
